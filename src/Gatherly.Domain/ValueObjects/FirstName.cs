91bfcc1c0dc983e7e32a4243c2b6fbc36753cf2e
m.peykanfar <m.peykanfar@PEYKANFAR-PC>
2026-04-09 18:23:40 +0330

    افزودن ولیو آبجکت FirstName

---
+++ src/Gatherly.Domain/ValueObjects/FirstName.cs
@@ -0,0 +1,36 @@
+﻿using Gatherly.Domain.Primitives;
+using Gatherly.Domain.Shared;
+
+namespace Gatherly.Domain.ValueObjects;
+
+public sealed class FirstName : ValueObject
+{
+    public const int MaxLength = 50;
+
+    public FirstName(string value)
+    {
+        Value = value;
+    }
+
+    public string Value { get; }
+
+    public static Result<FirstName> Create(string firstName)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+        {
+            return Result.Failure<FirstName>(new Error("FirstName.Empty", "FirstName is empty."));
+        }
+
+        if (firstName.Length > MaxLength)
+        {
+            return Result.Failure<FirstName>(new Error("FirstName.TooLong", "FirstName is too long."));
+        }
+
+        return new FirstName(firstName);
+    }
+
+    public override IEnumerable<Object> GetAtomicValues()
+    {
+        yield return Value;
+    }
+}
