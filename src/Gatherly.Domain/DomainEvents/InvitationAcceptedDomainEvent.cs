382ad9779a3bc5ded56f1048bae9a1b6440803ed
m.peykanfar <m.peykanfar@PEYKANFAR-PC>
2026-04-10 16:15:00 +0330

    افزودن رخداد پذیرش دعوت و ارسال ایمیل در واکنش به آن

---
+++ src/Gatherly.Domain/DomainEvents/InvitationAcceptedDomainEvent.cs
@@ -0,0 +1,7 @@
+﻿using Gatherly.Domain.Primitives;
+
+namespace Gatherly.Domain.DomainEvents;
+
+public sealed record class InvitationAcceptedDomainEvent(Guid InvitationId, Guid GatheringId) : IDomainEvent
+{
+}
--- src/Gatherly.Domain/Primitives/AggregateRoot.cs
+++ src/Gatherly.Domain/Primitives/AggregateRoot.cs
@@ -2,7 +2,14 @@
 
 public class AggregateRoot : Entity
 {
+    private readonly List<IDomainEvent> _domainEvents = new();
+
     protected AggregateRoot(Guid id) : base(id)
     {
+    }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
     }
 }
